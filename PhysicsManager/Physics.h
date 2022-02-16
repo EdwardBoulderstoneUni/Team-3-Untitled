a4c2b41f1ec46e8ee24aecdab70f60fb2b97ca1e
JackGHopkins <45544860+JackGHopkins@users.noreply.github.com>
2022-02-16 14:17:50 +0000

    Remade Physics Project to fix compile issues

---
+++ PhysicsManager/Physics.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PxPhysicsAPI.h"
+#include "../CSC8503/GameTech/TutorialGame.h"
+
+namespace NCL {
+	class Physics
+	{
+	public:
+		Physics();
+		~Physics();
+	private:
+	};
+}
