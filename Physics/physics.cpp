254fea17857d196ac574a4944de3eb4b3507a2b7
Edward <E.Boulderstone@newcastle.ac.uk>
2022-02-18 11:40:32 +0000

    Revert "pull conflict"
    This reverts commit df51eb4a0bf3863d3e4c52b467fcb46f0b3f7132.

--- Physics/physics.cpp
+++ Physics/physics.cpp
@@ -10,48 +10,20 @@ using namespace physx;
 PxReal stackZ = 10.0f;
 
 TutorialGame* game;
-
-Physics::Physics() {
-
-}
-Physics::~Physics()
+namespace NCL
 {
+	Physics::Physics() {
 
+	}
+	Physics::~Physics()
+	{
+
+	}
+
+	void Physics::Update(float dt) {
+
+	}
 }
-
-void Physics::Update(float dt) {
-
-}
-
-PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0))
-{
-	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
-	dynamic->setAngularDamping(0.5f);
-	dynamic->setLinearVelocity(velocity);
-	gScene->addActor(*dynamic);
-	return dynamic;
-}
-
-void Physics::createStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
-{
-	
-}
-
-void Physics::initPhysics(bool interactive)
-{
-		
-}
-
-void Physics::stepPhysics(bool /*interactive*/)
-{
-		
-}
-
-void Physics::cleanupPhysics(bool /*interactive*/)
-{
-		
-}
-
 
 
 //	void AddFloorToWorld(const PxTransform& t, PxVec3 fullExtents) {
--- Physics/physics.h
+++ Physics/physics.h
@@ -2,7 +2,7 @@
 #include "PxPhysicsAPI.h"
 #include "../CSC8503/GameTech/TutorialGame.h"
 
-namespace physx {
+namespace NCL {
 	class Physics
 	{
 	public:
@@ -11,17 +11,5 @@ namespace physx {
 
 		void update(float dt);
 	private:
-
-		PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0));
-
-		void createStack(const PxTransform& t, PxU32 size, PxReal halfExtent);
-
-		void initPhysics(bool interactive);
-
-		void stepPhysics(bool /*interactive*/);
-
-		void cleanupPhysics(bool /*interactive*/);
-
-
 	};
 }
