57d9a04085b790cc5a31d8ebc75a2130bb939ffa
AnnieXxu <c1030208@newcastle.ac.uk>
2022-02-24 15:08:07 +0000

    latested physic

--- Physics/PhysXobject.cpp
+++ Physics/PhysXobject.cpp
@@ -3,9 +3,17 @@
 using namespace physx;
 
 PhysXobject::PhysXobject(PxRigidActor* act, PxMaterial* m) {
-
+	actor = act;
+	material = m;
+	if (actor->is<PxRigidDynamic>()) {
+		PxRigidDynamic* body = (PxRigidDynamic*)act;
+		body->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);		//Enable CCD so high v objects don't move through each other
+	}
 }
 
 PhysXobject::~PhysXobject() {
-
+	if (actor != NULL) {
+		actor->release();
+		material->release();
+	}
 }
