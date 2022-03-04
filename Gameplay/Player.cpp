d7008d32b5b1e00c22ea588834656853e6a10ec7
JackGHopkins <45544860+JackGHopkins@users.noreply.github.com>
2022-03-04 17:44:01 +0000

    Added Ability Container

--- Gameplay/Player.cpp
+++ Gameplay/Player.cpp
@@ -2,9 +2,9 @@
 
 namespace NCL {
 	namespace CSC8503 {
-		Player::Player(PlayerRole colour, Vector3 transform) {
+		Player::Player(PlayerRole colour, AbilityContainer aCont) {
 			pColour = colour;
-			AssignRole();
+			AssignRole(aCont);
 		}
 
 		Player::~Player() {
@@ -24,24 +24,24 @@ namespace NCL {
 
 		}
 
-		void Player::AssignRole() {
+		void Player::AssignRole(AbilityContainer aCont) {
 			std::string colour;
 
 			switch (pColour) {
 			case Red:
 				colour = "Red";
-				abilities[0] = new Ability(colour.c_str(), 0);
-				abilities[1] = new Ability(colour.c_str(), 1);
+				abilities[0] = aCont.allAbilities[0];
+				abilities[1] = aCont.allAbilities[1];
 				break;
 			case Green:
 				colour = "Green";
-				abilities[0] = new Ability(colour.c_str(), 0);
-				abilities[1] = new Ability(colour.c_str(), 1);
+				abilities[0] = aCont.allAbilities[2];
+				abilities[1] = aCont.allAbilities[3];
 				break;
 			case Blue:
 				colour = "Blue";
-				abilities[0] = new Ability(colour.c_str(), 0);
-				abilities[1] = new Ability(colour.c_str(), 1);
+				abilities[0] = aCont.allAbilities[4];
+				abilities[1] = aCont.allAbilities[5];
 				break;
 			}
 		}
--- Gameplay/Player.h
+++ Gameplay/Player.h
@@ -1,19 +1,20 @@
 #pragma once
 #include "../CSC8503/CSC8503Common/GameObject.h"
 #include "Ability.h"
+#include "AbilityContainer.h"
 #include "ePlayerRole.h"
 
 namespace NCL {
 	namespace CSC8503 {		
 		class Player : GameObject {
 		public:
-			Player(PlayerRole colour, Vector3 transform);
+			Player(PlayerRole colour, AbilityContainer aCont);
 			~Player();
 
 			void Move();
 			void Jump();
 			void Shoot();
-			void AssignRole();
+			void AssignRole(AbilityContainer aCont);
 
 		private:
 
@@ -22,6 +23,7 @@ namespace NCL {
 			
 			// t is short for timer (cooldowns)
 			float tDash;
+			float dashCooldown;
 			float tAbility1;
 			float tAbility2;
 			float tDeath;
