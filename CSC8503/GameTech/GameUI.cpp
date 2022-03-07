fe9fbdfd4fd7fc864ee387c99253c7eb0201550e
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-07 12:08:19 +0000

    Removed redundant include

--- CSC8503/GameTech/GameUI.cpp
+++ CSC8503/GameTech/GameUI.cpp
@@ -1,7 +1,6 @@
 #include "GameUI.h"
 #include "../../Common/Win32Window.h"
 #include "../../Common/Window.h"
-#include "../../imgui/imgui.h"
 #include "../../imgui/backends/imgui_impl_win32.h"
 #include "../../imgui/backends/imgui_impl_opengl3.h"
 
