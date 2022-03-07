b2a3c089b7229382f9a25432794ee7ec75cc4e06
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-07 12:20:49 +0000

    Moved includes to .h to catch issues with multiple includes

--- CSC8503/GameTech/GameUI.h
+++ CSC8503/GameTech/GameUI.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
 #include "GameMenu.h"
+#include "../../Common/Win32Window.h"
+#include "../../imgui/backends/imgui_impl_win32.h"
+#include "../../imgui/backends/imgui_impl_opengl3.h"
 
 struct ExInputResult;
 
