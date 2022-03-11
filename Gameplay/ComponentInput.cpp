a631a6148c17ae5b649987d5510173add25c25fb
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-11 11:45:07 +0000

    Revert "Removed needless include and method"
    This reverts commit f215bbf4b3c6fa04db9ab30d7e4a239962b163d1.

--- Gameplay/ComponentInput.cpp
+++ Gameplay/ComponentInput.cpp
@@ -1,4 +1,8 @@
 #include "ComponentInput.h"
+#include "Player.h"
+void ComponentInput::Init() {
+}
+
 void ComponentInput::Update(float dt) {
 	if (userInterface->button_down(jump))
 		ButtonCallback[jump]();
@@ -6,7 +10,7 @@ void ComponentInput::Update(float dt) {
 		ButtonCallback[dash]();
 	if (userInterface->button_down(reload))
 		ButtonCallback[reload]();
-	if (userInterface->get_movement() != NCL::Maths::Vector2(0,0))
+	if (userInterface->get_movement() != Vector2(0,0))
 		MovCallback(userInterface->get_movement());
 	if (userInterface->button_down(attack))
 		ButtonCallback[attack]();
