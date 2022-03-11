bd8f4e9499f039707aa2c9d4a3e4272fcb55f3ca
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-11 11:45:04 +0000

    Revert "Made userInterface orginate from window"
    This reverts commit 1d1a92bc8ecc4f0d36076ae6e52ecb5666adc465.

--- Gameplay/ComponentInput.h
+++ Gameplay/ComponentInput.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "Component.h"
-#include "../Common/Window.h"
+#include "../Common/UserInterface.h"
 #include <functional>
-
 class ComponentInput: public Component {
 public:
-	ComponentInput() : userInterface(NCL::Window::GetInterface())
-	{ 
-		type = ComponentType::Input;
-
+	ComponentInput() { 
+		type = ComponentType::Input; 
+		userInterface = nullptr;
 		MovCallback = nullptr;
 	}
+	void Init() override;
 	void Update(float dt) override;
-	const UserInterface* userInterface;
+	UserInterface* userInterface;
 	std::function<void()> ButtonCallback[max_input];
 	std::function<void(NCL::Maths::Vector2 dir)> MovCallback;
 	std::function<void(float dt)> UpdateCallback;
+private:
 };
