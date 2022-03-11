bcc772d4a51e73d0324ab57baf0f67e2da48cf14
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-11 11:50:45 +0000

    Removed useless method and userInterface update

--- Gameplay/ComponentInput.cpp
+++ Gameplay/ComponentInput.cpp
@@ -1,19 +1,15 @@
 #include "ComponentInput.h"
-#include "Player.h"
-void ComponentInput::Init() {
-}
 
-void ComponentInput::Update(float dt) {
-	if (userInterface->button_down(jump))
-		ButtonCallback[jump]();
-	if (userInterface->button_down(dash))
-		ButtonCallback[dash]();
-	if (userInterface->button_down(reload))
-		ButtonCallback[reload]();
-	if (userInterface->get_movement() != Vector2(0,0))
-		MovCallback(userInterface->get_movement());
-	if (userInterface->button_down(attack))
-		ButtonCallback[attack]();
-	UpdateCallback(dt);
-	userInterface->update(dt);
+void ComponentInput::Update(const float dt) {
+	if (user_interface->button_down(jump))
+		button_callback[jump]();
+	if (user_interface->button_down(dash))
+		button_callback[dash]();
+	if (user_interface->button_down(reload))
+		button_callback[reload]();
+	if (user_interface->get_movement() != NCL::Maths::Vector2(0,0))
+		mov_callback(user_interface->get_movement());
+	if (user_interface->button_down(attack))
+		button_callback[attack]();
+	update_callback(dt);
 }
