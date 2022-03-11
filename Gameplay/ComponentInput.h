1d1a92bc8ecc4f0d36076ae6e52ecb5666adc465
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-11 11:44:12 +0000

    Made userInterface orginate from window

--- Gameplay/ComponentInput.h
+++ Gameplay/ComponentInput.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "Component.h"
-#include "../Common/UserInterface.h"
+#include "../Common/Window.h"
 #include <functional>
+
 class ComponentInput: public Component {
 public:
-	ComponentInput() { 
-		type = ComponentType::Input; 
-		userInterface = nullptr;
+	ComponentInput() : userInterface(NCL::Window::GetInterface())
+	{ 
+		type = ComponentType::Input;
+
 		MovCallback = nullptr;
 	}
-	void Init() override;
 	void Update(float dt) override;
-	UserInterface* userInterface;
+	const UserInterface* userInterface;
 	std::function<void()> ButtonCallback[max_input];
 	std::function<void(NCL::Maths::Vector2 dir)> MovCallback;
 	std::function<void(float dt)> UpdateCallback;
-private:
 };
