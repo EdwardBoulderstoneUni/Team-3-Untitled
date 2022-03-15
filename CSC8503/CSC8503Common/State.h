ea325b9e5da290af300139751e611f4921ddf094
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-15 15:18:37 +0000

    Fixed build issues

--- CSC8503/CSC8503Common/State.h
+++ CSC8503/CSC8503Common/State.h
@@ -7,34 +7,17 @@ namespace NCL {
 		class State {
 			public:
 			State() {}
-			State(StateUpdateFunction function) : stateFunction(function) {};
-			virtual ~State() {};
+			State(StateUpdateFunction function) : stateFunction(function) {}
+			virtual ~State() {}
 
 			virtual void Update(float dt)
 			{
-				if (stateFunction)
+				if (stateFunction != nullptr)
 					stateFunction(dt);
-			};
+			}
 
 		protected:
-			StateUpdateFunciton stateFunction;
-		};
-
-		typedef void(*StateFunc)(void*);
-		class GenericState : public State {
-		public:
-			GenericState(StateFunc someFunc, void* someData) {
-				stateFunction = someFunc;
-				funcData = someData;
-			}
-			virtual void Update(float dt) {
-				if (funcData != nullptr) {
-					func(funcData);
-				}
-			}
-		protected:
-			StateFunc stateFunction;
-			void* funcData;
+			StateUpdateFunction stateFunction;
 		};
 	}
 }
