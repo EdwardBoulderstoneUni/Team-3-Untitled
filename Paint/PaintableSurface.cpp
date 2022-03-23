72e3f86a53f8514a2e731c17e9ca2834310a71c5
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-23 14:11:44 +0000

    Implemented non gameplay interface methods

--- Paint/PaintableSurface.cpp
+++ Paint/PaintableSurface.cpp
@@ -1,1 +1,19 @@
 #include "PaintableSurface.h"
+
+#include "PaintManager.h"
+
+PaintableSurface::PaintableSurface(const GameObject* surface) : render_object_(
+	dynamic_cast<PaintableRenderObject*>(surface->GetRenderObject()))
+{
+	type = Component::ComponentType::Paint;
+}
+
+void PaintableSurface::Paint(const NCL::Maths::Vector3& local_pos)
+{
+	PaintManager::paint(this, local_pos);
+}
+
+PaintableRenderObject* PaintableSurface::GetRenderObject() const
+{
+	return render_object_;
+}
