fc4fd10f96eab5c7e374a21c094dabd72f89e1cf
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-23 14:18:09 +0000

    Moved get colour interface to render object

--- Paint/PaintableRenderObject.h
+++ Paint/PaintableRenderObject.h
@@ -8,6 +8,7 @@ public:
 	PaintableRenderObject(NCL::CSC8503::Transform* parent_transform, NCL::MeshGeometry* mesh, NCL::TextureBase* tex, NCL::MeshMaterial* mat = nullptr);
 	~PaintableRenderObject();
 	void bind_shader_values(NCL::RendererBase* renderer) const override;
+	NCL::Vector4 get_colour_at(const NCL::Maths::Vector3& local_pos);
 	NCL::TextureBase* get_paint_dest() const;
 	NCL::TextureBase* get_mask() const;
 protected:
