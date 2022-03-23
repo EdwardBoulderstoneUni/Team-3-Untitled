a09f01648a8778eb8923624fef6324c2ab469854
c0089913 <p.v.k.jha2@newcastle.ac.uk>
2022-03-23 13:27:23 +0000

    adding sound presets to audio manager

--- AudioManager/AudioManager.cpp
+++ AudioManager/AudioManager.cpp
@@ -17,16 +17,16 @@ void AudioManager::Cleanup()
 	delete m_Instance;
 }
 
-void AudioManager::Play_Sound()
+void AudioManager::Play_Sound(SoundPreset preset)
 {
-	bool playing = false;
-	m_Result = m_Channel->isPlaying(&playing);
+	//bool playing = false;
+	//m_Result = m_Channel->isPlaying(&playing);
 
-	if (!playing)
-	{
-		m_Result = m_System->playSound(m_Sound, nullptr, false, &m_Channel);
-		ERRCHECK(m_Result);
-	}
+	//if (!playing)
+	//{
+	m_Result = m_System->playSound(m_Sounds.at(preset), nullptr, false, &m_Channel);
+	ERRCHECK(m_Result);
+	//}
 }
 
 void AudioManager::Update(float dt)
@@ -34,29 +34,6 @@ void AudioManager::Update(float dt)
 	m_Result = m_System->update();
 	ERRCHECK(m_Result);
 
-	unsigned int ms = 0;
-	unsigned int lenms = 0;
-	bool playing = false;
-	bool paused = false;
-	int channelsplaying = 0;
-
-	/*	if (m_Channel)
-		{
-			FMOD::Sound* currentsound = 0;
-
-			m_Result = m_Channel->isPlaying(&playing);
-
-			m_Result = m_Channel->getPaused(&paused);
-
-			m_Result = m_Channel->getPosition(&ms, FMOD_TIMEUNIT_MS);
-
-			m_Channel->getCurrentSound(&currentsound);
-			if (currentsound)
-			{
-				m_Result = currentsound->getLength(&lenms, FMOD_TIMEUNIT_MS);
-
-			}
-		}*/
 }
 
 AudioManager::AudioManager()
@@ -67,28 +44,37 @@ AudioManager::AudioManager()
 	m_Result = m_System->init(32, FMOD_INIT_NORMAL, extradriverdata);
 	ERRCHECK(m_Result);
 	m_Channel = nullptr;
-	m_Sound = nullptr;
 
+	LoadSoundFiles();
+}
 
-	//temp
-	std::string testSoundFilePath = NCL::Assets::SOUNDSDIR;
-	testSoundFilePath.append("fightmusic.mp3");
-	m_Result = m_System->createSound(testSoundFilePath.c_str(),
-		FMOD_2D, nullptr, &m_Sound);
+void AudioManager::LoadSoundFiles()
+{
+	
+}
+
+FMOD::Sound* AudioManager::GetSound(const char* fileName, bool loop, int frequency, int priority)
+{
+	std::string soundDirectory = NCL::Assets::SOUNDSDIR;
+	FMOD::Sound* sound = nullptr;
+
+	m_Result = m_System->createSound(soundDirectory.append(fileName).c_str(), FMOD_2D, nullptr, &sound);
 	ERRCHECK(m_Result);
 
-	m_Result = m_Sound->setMode(FMOD_LOOP_OFF);
-	/* drumloop.wav has embedded loop points which automatically makes looping turn on, */
+	m_Result = sound->setMode(loop ? FMOD_DEFAULT : FMOD_LOOP_OFF);
 	ERRCHECK(m_Result);
-	/* so turn it off here.  We could have also just put FMOD_LOOP_OFF in the above CreateSound call. */
 
-	m_Sound->setDefaults(12000, 128);
+	sound->setDefaults(frequency, priority);
+	return sound;
 }
 
 AudioManager::~AudioManager()
 {
-	m_Result = m_Sound->release();
-	ERRCHECK(m_Result);
+	for (auto& sound : m_Sounds)
+	{
+		m_Result = sound.second->release();
+		ERRCHECK(m_Result);
+	}	
 	m_Result = m_System->close();
 	ERRCHECK(m_Result);
 	m_Result = m_System->release();
--- AudioManager/AudioManager.h
+++ AudioManager/AudioManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <map>
 namespace FMOD
 {
 	class System;
@@ -10,18 +11,26 @@ class AudioManager
 {
 	static AudioManager* m_Instance;
 	AudioManager();
-
-
+	void LoadSoundFiles();
+	FMOD::Sound* GetSound(const char* fileName, bool loop = false, int frequency = 12000, int priority = 128);
+	enum SoundPreset;
 	FMOD::System* m_System;
 	FMOD::Channel* m_Channel;
-	FMOD::Sound* m_Sound;
 
+	std::map<SoundPreset, FMOD::Sound*>m_Sounds;
 public:
+	enum SoundPreset
+	{
+		SoundPreset_MainMenu,
+		SoundPreset_InGame,
+		SoundPreset_Collision,
+		SoundPreset_MAX
+	};
 	~AudioManager();
 	static AudioManager& GetInstance() { return *m_Instance; }
 	static void Startup();
 	static void Cleanup();
-	//test function, replace with proper implementation in future
-	void Play_Sound();
+	
+	void Play_Sound(SoundPreset preset);
 	void Update(float dt);
 };
