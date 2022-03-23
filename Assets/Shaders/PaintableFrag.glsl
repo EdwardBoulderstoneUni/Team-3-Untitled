563f35778fd7af06d2d0847d183090381e436211
Edward <E.Boulderstone@newcastle.ac.uk>
2022-03-23 14:30:53 +0000

    Pulled missing methods from external shader

--- Assets/Shaders/PaintableFrag.glsl
+++ Assets/Shaders/PaintableFrag.glsl
@@ -24,6 +24,17 @@ in Vertex
 
 out vec4 fragColor;
 
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+10.0)*x);
+}
 
 float snoise(vec2 v)
   {
