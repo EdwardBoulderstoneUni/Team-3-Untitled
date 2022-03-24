358778ac7aba91a062de456eb6e0650d85de0ad5
JackGHopkins <45544860+JackGHopkins@users.noreply.github.com>
2022-03-24 13:15:05 +0000

    "Loading Screen"

--- CSC8503/GameTech/GameState.cpp
+++ CSC8503/GameTech/GameState.cpp
@@ -88,10 +88,13 @@ PushdownState::PushdownResult PauseState::OnUpdate(float dt, PushdownState** new
 #pragma region Load State
 LoadState::LoadState() {
 	loadingGame = true;
+
 	world = new GameWorld();
 	world->SetMainCamera(new Camera(0, 0, Vector3(-50, 0, -50)));
 	Window::GetWindow()->GetRenderer()->SetWorld(world);
 	renderer = dynamic_cast<GameTechRenderer*>(Window::GetWindow()->GetRenderer());
+	renderer->DrawString("Loading: 0%" , Vector2(5, 95));
+	Update(0.01f);
 
 	mesh = new OGLMesh();
 	mesh->GenerateSquare(mesh);
@@ -99,18 +102,24 @@ LoadState::LoadState() {
 	mesh->UploadToGPU(renderer);
 
 	object = new GameObject();
-
+	Update(0.01f);
 	object->SetRenderObject(new RenderObject(&object->GetTransform(),
 		mesh,
 		(OGLTexture*)TextureLoader::LoadAPITexture("checkerboard.png"),
 		new OGLShader("GameTechVert.glsl", "GameTechFrag.glsl")));
 
 	world->AddGameObject(object);
-	loadingThread = std::thread([this]() {	
-		ShaderManager::GetInstance()->Init();
-		AssetManager::GetInstance()->Init();
-		loadingGame = false; 
-	});
+	renderer->DrawString("Loading: 5%" , Vector2(5, 95));
+	Update(0.01f);
+
+	
+	ShaderManager::GetInstance()->Init();
+	renderer->DrawString("Loading: 35%" , Vector2(5, 95));
+	Update(0.01f);
+	AssetManager::GetInstance()->Init();
+	renderer->DrawString("Loading: 100%" , Vector2(5, 95));
+	Update(0.01f);
+	loadingGame = false; 
 }
 
 LoadState::~LoadState() {
@@ -121,13 +130,9 @@ LoadState::~LoadState() {
 }
 
 void LoadState::LoadGame() {
-
-
-
 	while (loadingGame) {
 		Update(0.01f);
 	}
-	loadingThread.join();
 }
 
 void LoadState::Update(float dt) {
